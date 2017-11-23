8465dab8bb2b4cfeac9abc9a0f434af0ba7b9aab
zhihuaye <zhihua.ye@spreadtrum.com>
2017-11-23 15:23:15 +0800

    parser basically works

--- lib/configconst.py
+++ lib/configconst.py
@@ -4,12 +4,65 @@
 conf parser constants
 """
 
+
 class ConfigSection:
     desc="description"
     dep="dependency"
     param="paramters"
 
+
 class ConfigType:
     master="master"
     slave="slave"
+
+class ConfigException(Exception):
+    def __init__(self, message):
+        super(ConfigException, self).__init__(message)
+        self.message = message
+
+
+def configHasSection(config, sectionname):
+    if sectionname in config:
+        return True
+    else:
+        return False
+
+
+def isFile(fname, ftype):
+    if fname:
+        fields = fname.split('.')
+        postfix = fields[1]
+
+        if postfix == ftype:
+            return True
+        else:
+            return False
+
+
+def checkFlistType(flist, ftype):
+    for index, onefile in enumerate(flist):
+        if isFile(onefile, ftype) is False:
+            return False
+    return True
+
+def convertList(value):
+    if not isinstance(value, list):
+        ret = list()
+        ret.append(value)
+        return ret
+    else:
+        return value
+
+def isMasterConf(ftype):
+    if ftype == ConfigType.master:
+        return True
+    else:
+        return False
+
+
+def isSlaveConf(ftype):
+    if ftype == ConfigType.slave:
+        return True
+    else:
+        return False
 
--- lib/utconfparser.py
+++ lib/utconfparser.py
@@ -14,44 +14,114 @@ class ConfAttr(dict):
     def __init__(self, *arg, **kw):
         #define some default values
         self['type'] = ConfigType.slave
+        self['inis'] = list()
+        self['xmls'] = list()
+        self['timeout'] = 5
+        self['port'] = 5065
         super(ConfAttr, self).__init__(*arg, **kw)
 
+    def dump(self):
+        for key, value in self.iteritems():
+            print 'key is %s, value is %s' % (key,value)
 
 class utconfparser:
-    def __init__(self, configfile='', configspec=''):
+    def __init__(self, brickdir='', confdir='', configfile='', configspec=''):
         self.attr = ConfAttr()
+        #brick xml dir
+        self.brickdir = brickdir
+        #case def dir
+        self.confdir = confdir
+
         self.logger = logConf()
-        #ut config default should be slave
-
+        #sub config may still exist.
+        self.subconf = list()
+        self.config = None
         try:
-            config = ConfigObj(configfile, configspec=configspec, file_error=True)
-            #actually ConfigObj has its own validator
-            #But seems need to reinvent the wheel
-            if self.validator(config):
-                if 'type' in config[ConfigSection.desc]:
-                    self.attr['type'] = config[ConfigSection.desc]['type']
-
-            else:
-                self.logger.logger.error("Invalid Config file.")
-
+            conffile = os.path.realpath(confdir + '/' + configfile)
+            config = ConfigObj(conffile, configspec=configspec, file_error=True)
+            self.config = config
+            #remove validate function, just raise exception when keyerror
         except:
             etype = sys.exc_info()[0]
             evalue = sys.exc_info()[1]
-            self.logger.logger.error("Unexpected error:"+ etype +' '+evalue)
+            self.logger.logger.error("Unexpected error:"+ str(etype) +' '+str(evalue))
 
-    def validator(self, config):
-        #quite Simple and Stupid
-        if config:
-            if ConfigSection.desc in config:
-                if ConfigSection.dep in config:
-                    if 'inis' in config[ConfigSection.dep] or 'xmls' in config[ConfigSection.dep]:
-                        return True
-                else:
-                    self.logger.logger.error("no 'dependency' in config file")
-        return False
 
-    def dumpAttr(self):
+    def getmaster(self):
+        '''
+
+        :return:
+        '''
+        try:
+            config = self.config
+            self.attr['inis'] = convertList(config[ConfigSection.dep]['inis'])
+            print 'inis is ' + str(self.attr['inis'])
+            if checkFlistType(self.attr['inis'], 'ini'):
+                #parse subconfs
+                for index, ini in enumerate(self.attr['inis']):
+                    inipath = os.path.realpath(self.confdir + '/' + ini)
+                    inidirname = os.path.dirname(inipath)
+                    subcon = utconfparser(brickdir=self.brickdir, confdir=inidirname, configfile=ini)
+                    subcon.getattr()
+                    self.subconf.append(subcon)
+
+            else:
+                emsg = "type is not all ini files"
+                raise ConfigException(emsg)
+        except:
+            etype = sys.exc_info()[0]
+            evalue = sys.exc_info()[1]
+            self.logger.logger.error("Unexpected error:"+ str(etype) + ' ' +str(evalue))
+
+    def getslave(self):
+        config = self.config
+        try:
+            self.attr['xmls'] = convertList(config[ConfigSection.dep]['xmls'])
+
+            if checkFlistType(self.attr['xmls'], 'xml'):
+                #collect xmls
+                pass
+            else:
+                emsg = "type is not all xml files"
+                raise ConfigException(emsg)
+
+            #collect port
+            #collect timeout
+        except:
+            etype = sys.exc_info()[0]
+            evalue = sys.exc_info()[1]
+            self.logger.logger.error("Unexpected error:"+ str(etype) + ' ' +str(evalue))
+
+
+    def getattr(self):
+        """
+        master, slave need different attr
+        master need: inis
+        slave need:  xmls
+        :return:
+        """
+        config = self.config
+
+        try:
+            self.attr['type'] = config[ConfigSection.desc]['type']
+            if isMasterConf(self.attr['type']):
+                self.getmaster()
+            elif isSlaveConf(self.attr['type']):
+                self.getslave()
+            else:
+                emsg = "Not valid Type"
+                self.logger.logger.error(emsg)
+                raise ConfigException(emsg)
+            self.attr.dump()
+        except:
+            etype = sys.exc_info()[0]
+            evalue = sys.exc_info()[1]
+            self.logger.logger.error("Unexpected error:"+ str(etype) +' '+str(evalue))
+
+
+    def collectsubconf(self):
         pass
 
 if __name__ == "__main__":
-    up = utconfparser(configfile="../cases/mt/config.ini")
+    up = utconfparser(brickdir="../cases/bricks/", confdir='../cases/mt/', configfile="config.ini")
+    up.getattr()
